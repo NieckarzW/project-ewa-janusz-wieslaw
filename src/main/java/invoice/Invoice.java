d8aacbf21ce7fbc9c68630e1a8823409d6baf9ef
WieslawN <nieckarzw@gmail.com>
2019-03-10 10:18:41 +0100

    Add class invoice and Vat

---
+++ src/main/java/invoice/Invoice.java
@@ -0,0 +1,108 @@
+package invoice;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Objects;
+
+public class Invoice {
+    private Long id;
+    private String number;
+    private LocalDateTime date;
+    private LocalDate dueDate;
+    private Company toCompany;
+    private Company fromCompany;
+    private List<InvoiceEntry> invoiceEntries;
+
+    public Invoice(long id, String number, LocalDateTime date, LocalDate dueDate, Company toCompany, Company fromCompany) {
+        this.id = id;
+        this.number = number;
+        this.date = date;
+        this.dueDate = dueDate;
+        this.toCompany = toCompany;
+        this.fromCompany = fromCompany;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(LocalDate dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public Company getToCompany() {
+        return toCompany;
+    }
+
+    public void setToCompany(Company toCompany) {
+        this.toCompany = toCompany;
+    }
+
+    public Company getFromCompany() {
+        return fromCompany;
+    }
+
+    public void setFromCompany(Company fromCompany) {
+        this.fromCompany = fromCompany;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Invoice invoice = (Invoice) o;
+        return id == invoice.id &&
+                Objects.equals(number, invoice.number) &&
+                Objects.equals(date, invoice.date) &&
+                Objects.equals(dueDate, invoice.dueDate) &&
+                Objects.equals(toCompany, invoice.toCompany) &&
+                Objects.equals(fromCompany, invoice.fromCompany);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, number, date, dueDate, toCompany, fromCompany);
+    }
+
+    @Override
+    public String toString() {
+        return "Invoice{" +
+                "id=" + id +
+                ", number='" + number + '\'' +
+                ", date=" + date +
+                ", dueDate=" + dueDate +
+                ", toCompany=" + toCompany +
+                ", fromCompany=" + fromCompany +
+                ", invoiceEntries=" + invoiceEntries +
+                '}';
+    }
+}
---
+++ src/main/java/invoice/Vat.java
@@ -0,0 +1,17 @@
+package invoice;
+
+import java.math.BigDecimal;
+
+public enum Vat {
+    VAT_0(new BigDecimal("0.00")),
+    VAT_5(new BigDecimal("0.05")),
+    VAT_8(new BigDecimal("0.08")),
+    VAT_23(new BigDecimal("0.23"));
+
+    private final BigDecimal rateValue;
+
+    Vat(BigDecimal rateValue) {
+        this.rateValue = rateValue;
+    }
+}
+
