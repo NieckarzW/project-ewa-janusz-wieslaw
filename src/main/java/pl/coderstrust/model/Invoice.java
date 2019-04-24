4af0d649349f37f4e481d2410b0f1eff4fb3c88e
WieslawN <nieckarzw@gmail.com>
2019-04-24 22:07:38 +0200

    create new branch

--- src/main/java/pl/coderstrust/model/Invoice.java
+++ src/main/java/pl/coderstrust/model/Invoice.java
@@ -1,5 +1,8 @@
 package pl.coderstrust.model;
 
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Objects;
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -8,10 +11,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
-import java.time.LocalDate;
-import java.util.List;
-import java.util.Objects;
-
 
 @Entity
 public final class Invoice {
