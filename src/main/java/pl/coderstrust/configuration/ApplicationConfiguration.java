0e640e2b9134345a608d15a89ad5f3279701c2f6
Pawel Zieba <zieba.pawel@gmail.com>
2019-05-21 21:39:33 +0200

    disable/enable hibernate autoconfiguration mechanism

--- src/main/java/pl/coderstrust/configuration/ApplicationConfiguration.java
+++ src/main/java/pl/coderstrust/configuration/ApplicationConfiguration.java
@@ -3,12 +3,19 @@ package pl.coderstrust.configuration;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @Configuration
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class,
+        DataSourceTransactionManagerAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class})
 @EnableAsync
 public class ApplicationConfiguration {
 
