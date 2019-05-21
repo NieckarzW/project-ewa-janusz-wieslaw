f38d1e2501fb6a04ba115358065e8a7a3578e9ae
Janusz <johnytoja@gmail.com>
2019-05-21 21:19:24 +0200

    adding Soap schema, mappers and endpoint

---
+++ src/main/java/pl/coderstrust/mappers/CompanyMapper.java
@@ -0,0 +1,32 @@
+package pl.coderstrust.mappers;
+
+
+import pl.coderstrust.soap.models.Company;
+
+public class CompanyMapper {
+
+  public static Company mapModelCompanyToSoapCompany(pl.coderstrust.model.Company modelCompany) {
+    Company soapCompany = new Company();
+    soapCompany.setId(modelCompany.getId());
+    soapCompany.setName(modelCompany.getName());
+    soapCompany.setAddress(modelCompany.getAddress());
+    soapCompany.setTaxId(modelCompany.getTaxId());
+    soapCompany.setAccountNumber(modelCompany.getAccountNumber());
+    soapCompany.setPhoneNumber(modelCompany.getPhoneNumber());
+    soapCompany.setEmail(modelCompany.getEmail());
+    return soapCompany;
+  }
+
+  public static pl.coderstrust.model.Company mapSoapCompanyToModelCompany(Company soapCompany) {
+    pl.coderstrust.model.Company modelCompany = new pl.coderstrust.model.Company(
+        Long.valueOf(soapCompany.getId()),
+        soapCompany.getName(),
+        soapCompany.getAddress(),
+        soapCompany.getTaxId(),
+        soapCompany.getAccountNumber(),
+        soapCompany.getPhoneNumber(),
+        soapCompany.getEmail());
+    return modelCompany;
+  }
+
+}
